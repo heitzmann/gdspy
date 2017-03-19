a7f37517d516408e02aff28ac1ac1db827481a03
Lucas H. Gabrielli <heitzmann@gmail.com>
2017-03-19 12:40:02 -0300

    Update paths

--- tools/gdspy_build_amd64.bat
+++ tools/gdspy_build_amd64.bat
@@ -1,11 +1,11 @@
 CALL "c:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" amd64
 cd \gdspy
-c:\WinPython-64bit-3.6.0.1Qt5b5\python-3.6.0.amd64\python.exe setup.py test
-c:\WinPython-64bit-3.6.0.1Qt5b5\python-3.6.0.amd64\python.exe setup.py bdist_wininst
-c:\WinPython-64bit-3.5.2.3Qt5\python-3.5.2.amd64\python.exe setup.py test
-c:\WinPython-64bit-3.5.2.3Qt5\python-3.5.2.amd64\python.exe setup.py bdist_wininst
+c:\WinPython-64bit-3.6.0.1Zero\python-3.6.0.amd64\python.exe setup.py test
+c:\WinPython-64bit-3.6.0.1Zero\python-3.6.0.amd64\python.exe setup.py bdist_wininst
+c:\WinPython-64bit-3.5.3.0Zero\python-3.5.3.amd64\python.exe setup.py test
+c:\WinPython-64bit-3.5.3.0Zero\python-3.5.3.amd64\python.exe setup.py bdist_wininst
 
 CALL "%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" amd64
 cd \gdspy
-c:\WinPython-64bit-2.7.10.3\python-2.7.10.amd64\python.exe setup.py test
-c:\WinPython-64bit-2.7.10.3\python-2.7.10.amd64\python.exe setup.py bdist_wininst
+c:\WinPython-64bit-2.7.13.0Zero\python-2.7.13.amd64\python.exe setup.py test
+c:\WinPython-64bit-2.7.13.0Zero\python-2.7.13.amd64\python.exe setup.py bdist_wininst
--- tools/gdspy_build_x86.bat
+++ tools/gdspy_build_x86.bat
@@ -1,11 +1,21 @@
 CALL "c:\Program Files (x86)\Microsoft Visual C++ Build Tools\vcbuildtools.bat" x86
+
 cd \gdspy
-c:\WinPython-32bit-3.6.0.1Qt5b5\python-3.6.0\python.exe setup.py test
-c:\WinPython-32bit-3.6.0.1Qt5b5\python-3.6.0\python.exe setup.py bdist_wininst
-c:\WinPython-32bit-3.5.2.3Qt5\python-3.5.2\python.exe setup.py test
-c:\WinPython-32bit-3.5.2.3Qt5\python-3.5.2\python.exe setup.py bdist_wininst
+
+c:\WinPython-32bit-3.6.0.1Zero\python-3.6.0\python.exe setup.py test
+
+c:\WinPython-32bit-3.6.0.1Zero\python-3.6.0\python.exe setup.py bdist_wininst
+
+c:\WinPython-32bit-3.5.3.0Zero\python-3.5.3\python.exe setup.py test
+
+c:\WinPython-32bit-3.5.3.0Zero\python-3.5.3\python.exe setup.py bdist_wininst
+
+
 
 CALL "%LOCALAPPDATA%\Programs\Common\Microsoft\Visual C++ for Python\9.0\vcvarsall.bat" x86
+
 cd \gdspy
-c:\WinPython-32bit-2.7.10.3\python-2.7.10\python.exe setup.py test
-c:\WinPython-32bit-2.7.10.3\python-2.7.10\python.exe setup.py bdist_wininst
+
+c:\WinPython-32bit-2.7.13.0Zero\python-2.7.13\python.exe setup.py test
+
+c:\WinPython-32bit-2.7.13.0Zero\python-2.7.13\python.exe setup.py bdist_wininst
