19b07f79911c1ffd6eec4da4367ccb1f69a154c9
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2017-12-05 12:45:30 -0200

    Integrate Sphinx into distutils

--- docs/conf.py
+++ docs/conf.py
@@ -16,11 +16,17 @@
 import sys
 import os
 import shlex
+import distutils.command.build
+from distutils.dist import Distribution
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../../gdspy'))
+b = distutils.command.build.build(Distribution())
+b.initialize_options()
+b.finalize_options()
+sys.path.insert(0, os.path.abspath('..' + os.sep + b.build_platlib))
+print(sys.path)
 
 # -- General configuration ------------------------------------------------
 
