cedbcadbb63f70abdc9ccbe99354bf701fc033c2
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2017-12-05 22:16:17 -0200

    Cleanup

--- tools/compile_extensions.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd gdspy
-gcc -O3 -fPIC -shared -I/usr/include/python3.6m -L/usr/lib -lpython3.6m -o boolext.so boolext.c
-g++ -O3 -fPIC -shared -I/usr/include/python3.6m -L/usr/lib -lpython3.6m -o clipper.so clipper.cpp
-cd -
