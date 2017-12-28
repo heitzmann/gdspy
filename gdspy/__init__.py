050737000a3752e96e8ad099eaccf2f409a8688e
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2017-12-28 17:01:43 -0200

    yapf fix

--- gdspy/__init__.py
+++ gdspy/__init__.py
@@ -3599,7 +3599,8 @@ class GdsLibrary(object):
             # STRANS
             elif record[0] == 0x1a:
                 kwargs['x_reflection'] = ((int(record[1][0]) & 0x8000) > 0)
-                if (int(record[1][0]) & 0x0006) and record[0] not in emitted_warnings:
+                if (int(record[1][0]) &
+                        0x0006) and record[0] not in emitted_warnings:
                     warnings.warn(
                         "[GDSPY] Absolute magnification or rotation of "
                         "references is not supported.  Transformations will "
