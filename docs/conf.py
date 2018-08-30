0d91a158dc2f6aa38e13a61cbebfe20784d6a72c
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2018-08-30 14:05:18 -0300

    Update docs config

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,20 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    #'canonical_url': '',
+    #'analytics_id': '',
+    #'logo_only': False,
+    'display_version': True,
+    #'prev_next_buttons_location': 'bottom',
+    #'style_external_links': False,
+    #'vcs_pageview_mode': '',
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': -1,
+    #'includehidden': True,
+    #'titles_only': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
