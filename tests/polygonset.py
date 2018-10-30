6424009f94c25fd8badd73a2614d56923d23dab7
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2018-10-30 10:19:29 -0300

    Fix broken tests

--- tests/polygonset.py
+++ tests/polygonset.py
@@ -14,23 +14,23 @@ import gdspy
 def test_fillet():
     r = gdspy.Rectangle((0, 0), (1, 1))
     r.fillet(0.25, points_per_2pi=64)
-    gdspy.Cell('1').add(r)
+    #gdspy.Cell('1').add(r)
     assert len(r.polygons) == 1
     assert r.polygons[0].shape[0] == 64
     r = gdspy.Rectangle((0, 0), (1, 1))
     r.fillet([[0.1, 0.2, 0.3, 0.4]], points_per_2pi=64)
-    gdspy.Cell('2').add(r)
+    #gdspy.Cell('2').add(r)
     assert len(r.polygons) == 1
     assert r.polygons[0].shape[0] == 64
     r = gdspy.Rectangle((0, 0), (1, 1))
     r.fillet([0.4, 0.3, 0.2, 0.1], points_per_2pi=64)
-    gdspy.Cell('3').add(r)
+    #gdspy.Cell('3').add(r)
     assert len(r.polygons) == 1
     assert r.polygons[0].shape[0] == 64
     r = gdspy.PolygonSet([[(0, 0), (0, 1), (0.5, 1), (0.5, 0)],
                           [(0.5, 0), (0.5, 1), (1, 1), (1, 0)]])
     r.fillet([0.1, [0.1, 0.3, 0.1, 0.3]], points_per_2pi=64)
-    gdspy.Cell('4').add(r)
+    #gdspy.Cell('4').add(r)
     assert len(r.polygons) == 2
     assert r.polygons[0].shape[0] == 64
     assert r.polygons[1].shape[0] == 64
@@ -38,8 +38,8 @@ def test_fillet():
     r = gdspy.PolygonSet([[(0, 0), (0, 1), (0.5, 1), (0.5, 0)],
                           [(0.5, 0), (0.5, 1), (1, 1), (1, 0)]])
     r.fillet([0.1, 0.3, 0.1, 0.3, 0.1, 0.3, 0.1, 0.3], points_per_2pi=64)
-    gdspy.Cell('5').add(r)
+    #gdspy.Cell('5').add(r)
     assert len(r.polygons) == 2
     assert r.polygons[0].shape[0] == 64
     assert r.polygons[1].shape[0] == 64
-    gdspy.LayoutViewer()
+    #gdspy.LayoutViewer()
