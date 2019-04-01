4fcc7bd05b9e1473ff6b4e54333e8d326f1de542
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2019-04-01 13:49:51 -0300

    Skeleton for testing polygon construction

--- tools/maketestgds.py
+++ tools/maketestgds.py
@@ -8,26 +8,15 @@
 ######################################################################
 
 import numpy
-import pytest
 import gdspy
 
 
-@pytest.fixture
-def target():
-    return gdspy.GdsLibrary(infile='tests/test.gds').cell_dict
+cell = gdspy.Cell('polygonset')
+p = gdspy.PolygonSet([
+    [(1, 1), (1, 2), (5, 3)],
+    [(0, 0.5), (0, 3), (-4, 1), (0, -1), (4, 0.5)],
+], 8, 9)
+cell.add(p)
 
-def assertsame(c1, c2):
-    d1 = c1.get_polygons(by_spec=True)
-    d2 = c2.get_polygons(by_spec=True)
-    for key in d1:
-        assert key in d2
-        result = gdspy.boolean(d1[key], d2[key], 'xor', precision=1e-6)
-        assert result is None
-
-def test_polygonset(target):
-    cell = gdspy.Cell('X', True).add(gdspy.PolygonSet([
-        [(1, 1), (1, 2), (5, 3)],
-        [(0, 0.5), (0, 3), (-4, 1), (0, -1), (4, 0.5)],
-    ], 8, 9))
-    assertsame(cell, target['polygonset'])
-
+gdspy.write_gds('tests/test.gds')
+gdspy.LayoutViewer()
