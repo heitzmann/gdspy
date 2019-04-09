b9d42ec4a394339727c7fd2bee0f44b85d548435
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2019-04-09 09:52:22 -0300

    Update test tool for new examples

--- tools/test.sh
+++ tools/test.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 VERSION=$1
 if [ $(which virtualenv$VERSION) ]; then
-	ENV=~/gdspy-env-$VERSION
+  ENV=~/gdspy-env-$VERSION
 
-	[ ! -d "$ENV" ] && virtualenv$VERSION --system-site-packages "$ENV"
-	[ -d "$ENV/lib/python$VERSION/site-packages/gdspy" ] && rm -r "$ENV/lib/python$VERSION/site-packages/gdspy"
+  [ ! -d "$ENV" ] && virtualenv$VERSION --system-site-packages "$ENV"
+  [ -d "$ENV/lib/python$VERSION/site-packages/gdspy" ] && rm -r "$ENV/lib/python$VERSION/site-packages/gdspy"
 
-	source "$ENV/bin/activate"
+  source "$ENV/bin/activate"
 
-	python setup.py build && python setup.py install
+  python setup.py build && python setup.py install
 
-	echo
-	echo Testing $VERSION
-	cd examples
+  echo
+  echo Testing $VERSION
+  cd docs/_static
   for i in *.py; do
     python "$i"
   done
-	cd -
-	cd tests
-	for i in *.py; do
-		python "$i"
-	done
-	cd -
+  cd -
 
-	deactivate
+  deactivate
 else
-	echo "Usage: ./test.sh version"
+  echo "Usage: ./test.sh version"
 fi
