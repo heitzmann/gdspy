799df5bd7260e814818a7e32de72790f7ff94a1e
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2019-04-24 17:21:23 -0300

    Fix _hobby and FlexPath

--- gdspy/__init__.py
+++ gdspy/__init__.py
@@ -234,93 +234,152 @@ def _hobby(points, angles=None, curl_start=1, curl_end=1, t_in=1, t_out=1, cycle
        `DOI: 10.1007/BF02187690 <https://doi.org/10.1007/BF02187690>`_
     """
     z = points[:, 0] + 1j * points[:, 1]
+    n = z.size
     if numpy.isscalar(t_in):
-        t_in = t_in * numpy.ones(len(z))
+        t_in = t_in * numpy.ones(n)
+    else:
+        t_in = numpy.array(t_in)
     if numpy.isscalar(t_out):
-        t_out = t_out * numpy.ones(len(z))
+        t_out = t_out * numpy.ones(n)
+    else:
+        t_out = numpy.array(t_out)
     if angles is None:
-        angles = [None] * len(z)
-    v = numpy.roll(z, -1) - z
-    d = numpy.abs(v)
-    delta = numpy.angle(v)
-    psi = (delta - numpy.roll(delta, 1) + numpy.pi) % (2 * numpy.pi) - numpy.pi
-    theta = numpy.zeros(len(z))
-    phi = numpy.zeros(len(z))
-    for i in range(len(angles)):
-        if angles[i] is not None:
-            theta[i] = angles[i] - delta[i]
-            phi[i] = delta[i - 1] - angles[i]
-    i = 0
-    while i < len(angles):
-        while i < len(angles) and angles[i] is not None:
-            i += 1
-        j = i
-        while j < len(angles) and angles[j] is None:
-            j += 1
-        if j > i:
-            n = 2 * (j - i)
-            m = numpy.zeros((n, n))
-            coeff = numpy.zeros(n)
-            coeff[::2] = -psi[i:j]
-            if i == 0:
-                if cycle:
-                    m[1, n - 2] = d[0] * t_out[0]**2 * t_out[1]
-                    m[1, 0] = -(1 - 3 * t_out[1]) * d[-1] * t_in[0]**2 * t_in[-1]
-                    m[1, 1] = (1 - 3 * t_in[-1]) * d[0] * t_out[0]**2 * t_out[1]
-                    m[1, 3] = -d[-1] * t_in[0]**2 * t_in[-1]
-                else:
-                    m[1, 0] = t_in[0]**3 * (1 - 3 * t_out[1]) - curl_start * t_out[1]**3
-                    m[1, 3] = t_in[0]**3 - curl_start * t_out[1]**3 * (1 - 3 * t_in[0])
+        angles = [None] * n
+    rotate = 0
+    if cycle and any(a is not None for a in angles):
+        while angles[rotate] is None:
+            rotate += 1
+        angles = [angles[(rotate + j) % n] for j in range(n + 1)]
+        z = numpy.hstack((numpy.roll(z, -rotate), z[rotate:rotate + 1]))
+        t_in = numpy.hstack((numpy.roll(t_in, -rotate), t_in[rotate:rotate + 1]))
+        t_out = numpy.hstack((numpy.roll(t_out, -rotate), t_out[rotate:rotate + 1]))
+        cycle = False
+    if cycle:
+        # Closed curve
+        v = numpy.roll(z, -1) - z
+        d = numpy.abs(v)
+        delta = numpy.angle(v)
+        psi = (delta - numpy.roll(delta, 1) + numpy.pi) % (2 * numpy.pi) - numpy.pi
+        coef = numpy.zeros(2 * n)
+        coef[:n] = -psi
+        m = numpy.zeros((2 * n, 2 * n))
+        i = numpy.arange(n)
+        i1 = (i + 1) % n
+        i2 = (i + 2) % n
+        ni = n + i
+        m[i, i] = 1
+        m[i, n + (i - 1) % n] = 1
+        # A_i
+        m[ni, i] = d[i1] * t_in[i2] * t_in[i1]**2
+        # B_{i+1}
+        m[ni, i1] = -d[i] * t_out[i] * t_out[i1]**2 * (1 - 3 * t_in[i2])
+        # C_{i+1}
+        m[ni, ni] = d[i1] * t_in[i2] * t_in[i1]**2 * (1 - 3 * t_out[i])
+        # D_{i+2}
+        m[ni, n + i1] = -d[i] * t_out[i] * t_out[i1]**2
+        sol = numpy.linalg.solve(m, coef)
+        theta = sol[:n]
+        phi = sol[n:]
+        w = numpy.exp(1j * (theta + delta))
+        a = 2**0.5
+        b = 1.0 / 16
+        c = (3 - 5**0.5) / 2
+        sintheta = numpy.sin(theta)
+        costheta = numpy.cos(theta)
+        sinphi = numpy.sin(phi)
+        cosphi = numpy.cos(phi)
+        alpha = a * (sintheta - b * sinphi) * (sinphi - b * sintheta) * (costheta - cosphi)
+        cta = z + w * d * ((2 + alpha) / (1 + (1 - c) * costheta + c * cosphi)) / (3 * t_out)
+        ctb = numpy.roll(z, -1) - numpy.roll(w, -1) * d * (
+            (2 - alpha) / (1 + (1 - c) * cosphi + c * costheta)) / (3 * numpy.roll(t_in, -1))
+    else:
+        # Open curve(s)
+        n = z.size - 1
+        v = z[1:] - z[:-1]
+        d = numpy.abs(v)
+        delta = numpy.angle(v)
+        psi = (delta[1:] - delta[:-1] + numpy.pi) % (2 * numpy.pi) - numpy.pi
+        theta = numpy.empty(n)
+        phi = numpy.empty(n)
+        i = 0
+        if angles[0] is not None:
+            theta[0] = angles[0] - delta[0]
+        while i < n:
+            j = i + 1
+            while j < n + 1 and angles[j] is None:
+                j += 1
+            if j == n + 1:
+                j -= 1
             else:
-                m[1, 0] = -(1 - 3 * t_out[i + 1]) * d[i - 1] * t_in[i]**2 * t_in[i - 1]
-                m[1, 1] = (1 - 3 * t_in[i - 1]) * d[i] * t_out[i]**2 * t_out[i + 1]
-                m[1, 3] = -d[i - 1] * t_in[i]**2 * t_in[i - 1]
-                coeff[1] = -d[i] * t_out[i]**2 * t_out[i + 1] * theta[i - 1]
-            if j == len(angles):
-                if cycle:
-                    m[n - 1, n - 4] = d[j - 1] * t_out[j - 1]**2 * t_out[0]
-                    m[n - 1, n - 2] = -(1 - 3 * t_out[0]) * d[j - 2] * t_in[j - 1]**2 * t_in[j - 2]
-                    m[n - 1, n - 1] = (1 - 3 * t_in[j - 2]) * d[j - 1] * t_out[j - 1]**2 * t_out[0]
-                    m[n - 1, 1] = -d[j - 2] * t_in[j - 1]**2 * t_in[j - 2]
-                else:
-                    m[n - 1, n - 4] = t_out[j - 1]**3 - curl_end * t_in[j - 2]**3 * (1 - 3 * t_out[j - 1])
-                    m[n - 1, n - 1] = t_out[j - 1]**3 * (1 - 3 * t_in[j - 2]) - curl_end * t_in[j - 2]**3
+                phi[j - 1] = delta[j - 1] - angles[j]
+                if j < n:
+                    theta[j] = angles[j] - delta[j]
+            # Solve open curve z_i, ..., z_j
+            nn = j - i
+            coef = numpy.zeros(2 * nn)
+            coef[1:nn] = -psi[i:j - 1]
+            m = numpy.zeros((2 * nn, 2 * nn))
+            if nn > 1:
+                ii = numpy.arange(nn - 1) #[0 .. nn-2]
+                i0 = i + ii               #[i .. j-1]
+                i1 = 1 + i0               #[i+1 .. j]
+                i2 = 2 + i0               #[i+2 .. j+1]
+                ni = nn + ii              #[nn .. 2*nn-2]
+                ii1 = 1 + ii              #[1 .. nn-1]
+                m[ii1, ii1] = 1
+                m[ii1, ni] = 1
+                # A_ii
+                m[ni, ii] = d[i1] * t_in[i2] * t_in[i1]**2
+                # B_{ii+1}
+                m[ni, ii1] = -d[i0] * t_out[i0] * t_out[i1]**2 * (1 - 3 * t_in[i2])
+                # C_{ii+1}
+                m[ni, ni] = d[i1] * t_in[i2] * t_in[i1]**2 * (1 - 3 * t_out[i0])
+                # D_{ii+2}
+                m[ni, ni + 1] = -d[i0] * t_out[i0] * t_out[i1]**2
+            if angles[i] is None:
+                to3 = t_out[0]**3
+                cti3 = curl_start * t_in[1]**3
+                # B_0
+                m[0, 0] = to3 * (1 - 3 * t_in[1]) - cti3
+                # D_1
+                m[0, nn] = to3 - cti3 * (1 - 3 * t_out[0])
             else:
-                m[n - 1, n - 4] = d[j - 1] * t_out[j - 1]**2 * t_out[j]
-                m[n - 1, n - 2] = -(1 - 3 * t_out[j]) * d[j - 2] * t_in[j - 1]**2 * t_in[j - 2]
-                m[n - 1, n - 1] = (1 - 3 * t_in[j - 2]) * d[j - 1] * t_out[j - 1]**2 * t_out[j]
-                coeff[n - 1] = d[j - 2] * t_in[j - 1]**2 * t_in[j - 2] * phi[j]
-            m[0, 0] = 1
-            m[0, 1] = 1
-            m[n - 2, n - 2] = 1
-            m[n - 2, n - 1] = 1
-            for k in range(i + 1, j - 1):
-                row = 2 * (k - i)
-                m[row, row] = 1
-                m[row, row + 1] = 1
-                m[row + 1, row - 2] = d[k] * t_out[k]**2 * t_out[k + 1]
-                m[row + 1, row] = -(1 - 3 * t_out[k + 1]) * d[k - 1] * t_in[k]**2 * t_in[k - 1]
-                m[row + 1, row + 1] = (1 - 3 * t_in[k - 1]) * d[k] * t_out[k]**2 * t_out[k + 1]
-                m[row + 1, row + 3] = -d[k - 1] * t_in[k]**2 * t_in[k - 1]
-            sol = numpy.linalg.solve(m, coeff)
-            theta[i:j] = sol[::2]
-            phi[i:j] = sol[1::2]
+                coef[0] = theta[i]
+                m[0, 0] = 1
+                m[0, nn] = 0
+            if angles[j] is None:
+                ti3 = t_in[n]**3
+                cto3 = curl_end * t_out[n - 1]**3
+                # A_{nn-1}
+                m[2 * nn - 1, nn - 1] = ti3 - cto3 * (1 - 3 * t_in[n])
+                # C_nn
+                m[2 * nn - 1, 2 * nn - 1] = ti3 * (1 - 3 * t_out[n - 1]) - cto3
+            else:
+                coef[2 * nn - 1] = phi[j - 1]
+                m[2 * nn - 1, nn - 1] = 0
+                m[2 * nn - 1, 2 * nn - 1] = 1
+            if nn > 1 or angles[i] is None or angles[j] is None:
+                sol = numpy.linalg.solve(m, coef)
+                theta[i:j] = sol[:nn]
+                phi[i:j] = sol[nn:]
             i = j
-    w = numpy.exp(1j * (theta + delta))
-    a = 2**0.5
-    b = 1.0 / 16
-    c = (3 - 5**0.5) / 2
-    sintheta = numpy.sin(theta)
-    costheta = numpy.cos(theta)
-    sinphi = numpy.sin(numpy.roll(phi, -1))
-    cosphi = numpy.cos(numpy.roll(phi, -1))
-    alpha = a * (sintheta - b * sinphi) * (sinphi - b * sintheta) * (costheta - cosphi)
-    cta = z + w * d * ((2 + alpha) / (1 + (1 - c) * costheta + c * cosphi)) / (3 * t_in)
-    ctb = numpy.roll(z, -1) - numpy.roll(w, -1) * d * (
-        (2 - alpha) / (1 + (1 - c) * cosphi + c * costheta)) / (3 * numpy.roll(t_out, -1))
-    if not cycle:
-        cta = cta[:-1]
-        ctb = ctb[:-1]
+        w = numpy.hstack((numpy.exp(1j * (delta + theta)),
+                          numpy.exp(1j * (delta[-1:] - phi[-1:]))))
+        a = 2**0.5
+        b = 1.0 / 16
+        c = (3 - 5**0.5) / 2
+        sintheta = numpy.sin(theta)
+        costheta = numpy.cos(theta)
+        sinphi = numpy.sin(phi)
+        cosphi = numpy.cos(phi)
+        alpha = a * (sintheta - b * sinphi) * (sinphi - b * sintheta) * (costheta - cosphi)
+        cta = z[:-1] + w[:-1] * d * (
+            (2 + alpha) / (1 + (1 - c) * costheta + c * cosphi)) / (3 * t_out[:-1])
+        ctb = z[1:] - w[1:] * d * (
+            (2 - alpha) / (1 + (1 - c) * cosphi + c * costheta)) / (3 * t_in[1:])
+        if rotate > 0:
+            cta = numpy.roll(cta, rotate)
+            ctb = numpy.roll(ctb, rotate)
     return (numpy.vstack((cta.real, cta.imag)).transpose(), numpy.vstack((ctb.real, ctb.imag)).transpose())
 
 
@@ -3087,7 +3146,7 @@ class FlexPath(object):
                                 a1 += 2 * numpy.pi
                         da = 0.5 * abs(a1 - a0)
                         d = r * numpy.tan(da) / (v0[0]**2 + v0[1]**2)**0.5
-                        np = max(2, 1 + int(da / numpy.arccos(1 - self.tolerance / r) + 0.5))
+                        np = max(2, 1 + int(da / numpy.arccos(1 - self.tolerance / (r + 0.5 * self.widths[0, ii])) + 0.5))
                         angles = numpy.linspace(a0, a1, np)
                         bpts = r * numpy.vstack((numpy.cos(angles), numpy.sin(angles))).T
                         bends.extend(bpts - bpts[0] + p1 - d * v0)
@@ -3096,14 +3155,14 @@ class FlexPath(object):
                     v0 = v1
                 bends.append(p1)
                 points = numpy.array(bends)
-            points = (points * multiplier).astype('>i4')
+            points = numpy.round(points * multiplier).astype('>i4')
             if points.shape[0] > 8191:
                 warnings.warn("[GDSPY] Paths with more than 8191 are not supported by the official GDSII specification.  This GDSII file might not be compatible with all readers.", stacklevel=4)
                 i0 = 0
                 while i0 < points.shape[0]:
-                    i1 = min(i0 + 8191, xy.shape[0])
+                    i1 = min(i0 + 8191, points.shape[0])
                     data.append(struct.pack('>2H', 4 + 8 * (i1 - i0), 0x1003))
-                    data.append(xy[i0:i1].tostring())
+                    data.append(points[i0:i1].tostring())
                     i0 = i1
             else:
                 data.append(struct.pack('>2H', 4 + 8 * points.shape[0], 0x1003))
@@ -3150,6 +3209,7 @@ class FlexPath(object):
         """
         self._polygon_dict = None
         self.points = self.points + numpy.array((dx, dy))
+        return self
 
     def rotate(self, angle, center=(0, 0)):
         """
@@ -4043,9 +4103,9 @@ class RobustPath(object):
                 warnings.warn("[GDSPY] Paths with more than 8191 are not supported by the official GDSII specification.  This GDSII file might not be compatible with all readers.", stacklevel=4)
                 i0 = 0
                 while i0 < points.shape[0]:
-                    i1 = min(i0 + 8191, xy.shape[0])
+                    i1 = min(i0 + 8191, points.shape[0])
                     data.append(struct.pack('>2H', 4 + 8 * (i1 - i0), 0x1003))
-                    data.append(xy[i0:i1].tostring())
+                    data.append(points[i0:i1].tostring())
                     i0 = i1
             else:
                 data.append(struct.pack('>2H', 4 + 8 * points.shape[0], 0x1003))
@@ -4096,6 +4156,7 @@ class RobustPath(object):
         for path in self.paths:
             for sub in path:
                 sub.x = _func_multadd(sub.x, None, offset)
+        return self
 
     def rotate(self, angle, center=(0, 0)):
         """
