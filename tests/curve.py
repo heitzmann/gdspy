af5a6c8e03843458369d1d2da1627fe97ba9b734
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2019-04-25 10:54:48 -0300

    Hobby tests

--- tests/curve.py
+++ tests/curve.py
@@ -13,7 +13,7 @@ import pytest
 import gdspy
 
 
-def test_curve(target):
+def test_hobby1(target):
     cell = gdspy.Cell('test', True)
     c = gdspy.Curve(0, 0, tolerance=1e-3)
     c.i([(1, 0), (1, 1), (0, 1)])
@@ -70,5 +70,76 @@ def test_curve(target):
     c = gdspy.Curve(4, 4, tolerance=1e-3)
     c.i([(1, 0), (1, 1), (0, 1)], angles=[-numpy.pi/4, 0, numpy.pi/2, -numpy.pi], cycle=True)
     cell.add(gdspy.Polygon(c.get_points(), layer=18))
-    assertsame(target['Curve'], cell)
+    assertsame(target['Hobby1'], cell)
+
+def test_hobby2(target):
+    cell = gdspy.Cell('test', True)
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)])
+    cell.add(gdspy.Polygon(c.get_points(), layer=1))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], curl_start=0)
+    cell.add(gdspy.Polygon(c.get_points(), layer=2))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], curl_end=0)
+    cell.add(gdspy.Polygon(c.get_points(), layer=3))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], curl_start=0, curl_end=0)
+    cell.add(gdspy.Polygon(c.get_points(), layer=4))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], angles=[numpy.pi/2, None, None, None, -numpy.pi/2],
+        curl_start=0, curl_end=0)
+    cell.add(gdspy.Polygon(c.get_points(), layer=5))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], angles=[None, 0, None, 0, None],
+        curl_start=0, curl_end=1)
+    cell.add(gdspy.Polygon(c.get_points(), layer=6))
+    assertsame(target['Hobby2'], cell)
+
+def test_hobby3(target):
+    cell = gdspy.Cell('test', True)
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)])
+    cell.add(gdspy.Polygon(c.get_points(), layer=1))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=2)
+    cell.add(gdspy.Polygon(c.get_points(), layer=2))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_out=2)
+    cell.add(gdspy.Polygon(c.get_points(), layer=3))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=2, t_out=2)
+    cell.add(gdspy.Polygon(c.get_points(), layer=4))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=[2, 1, 1, 1, 1], t_out=[1, 1, 1, 1, 2])
+    cell.add(gdspy.Polygon(c.get_points(), layer=5))
+    c = gdspy.Curve(0, 0, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=[1, 1, 2, 1, 1], t_out=[1, 2, 1, 1, 1])
+    cell.add(gdspy.Polygon(c.get_points(), layer=6))
+    assertsame(target['Hobby3'], cell)
+
+def test_hobby4(target):
+    cell = gdspy.Cell('test', True)
+    c = gdspy.Curve(0, 3, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], cycle=True)
+    cell.add(gdspy.Polygon(c.get_points(), layer=10))
+    c = gdspy.Curve(0, 3, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=[2, 1, 1, 1, 1],
+        t_out=[1, 1, 1, 1, 2], cycle=True)
+    cell.add(gdspy.Polygon(c.get_points(), layer=11))
+    c = gdspy.Curve(0, 3, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)], t_in=[1, 1, 2, 1, 1],
+        t_out=[1, 2, 1, 1, 1], cycle=True)
+    cell.add(gdspy.Polygon(c.get_points(), layer=12))
+    c = gdspy.Curve(0, 3, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)],
+        angles=[numpy.pi * 3/4, None, None, None, -numpy.pi * 3/4],
+        t_in=[2, 1, 1, 1, 1], t_out=[1, 1, 1, 1, 2], cycle=True)
+    cell.add(gdspy.Polygon(c.get_points(), layer=13))
+    c = gdspy.Curve(0, 3, tolerance=1e-3)
+    c.i([(1, 2), (2, 1), (3, 2), (4, 0)],
+        angles=[numpy.pi * 3/4, None, None, None, -numpy.pi * 3/4],
+        t_in=[1, 1, 1, 1, 1], t_out=[1, 1, 1, 1, 1], cycle=True)
+    cell.add(gdspy.Polygon(c.get_points(), layer=14))
+    assertsame(target['Hobby4'], cell)
 
