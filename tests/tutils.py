c049932f72647b584b6c4f39593ff10c51b1415f
Lucas Heitzmann Gabrielli <heitzmann@gmail.com>
2019-05-04 09:57:41 -0300

    Remove LayoutViewer from tests

--- tests/tutils.py
+++ tests/tutils.py
@@ -24,18 +24,18 @@ def assertsame(c1, c2, tolerance=1e-6):
         result = gdspy.boolean(d1[key], d2[key], 'xor', precision=1e-7,
                                layer=key[0], datatype=100)
         if result is not None:
-            c1.add(result)
-            c2.add(result)
             r1 = gdspy.boolean(d1[key], gdspy.offset(d2[key], tolerance, precision=1e-7),
                                'not', precision=1e-7, layer=key[0], datatype=99)
             r2 = gdspy.boolean(d2[key], gdspy.offset(d1[key], tolerance, precision=1e-7),
                                'not', precision=1e-7, layer=key[0], datatype=99)
-            if not (r1 is None and r2 is None):
-                if r1 is not None:
-                    c1.add(r1)
-                if r2 is not None:
-                    c2.add(r2)
-                gdspy.LayoutViewer(cells=[c1, c2])
+            #if not (r1 is None and r2 is None):
+            #    c1.add(result)
+            #    c2.add(result)
+            #    if r1 is not None:
+            #        c1.add(r1)
+            #    if r2 is not None:
+            #        c2.add(r2)
+            #    gdspy.LayoutViewer(cells=[c1, c2])
             assert r1 is None
             assert r2 is None
         else:
